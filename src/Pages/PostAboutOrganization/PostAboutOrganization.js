690c9774e4abdce1843711b8c0057eec47ec0fa7
mehedy201 <mehedy643@gmail.com>
2022-12-23 09:47:48 +0600

    create header part in post about organization page

--- src/Pages/PostAboutOrganization/PostAboutOrganization.js
+++ src/Pages/PostAboutOrganization/PostAboutOrganization.js
@@ -11,8 +11,8 @@ const PostAboutOrganization = () => {
                     </div>
                 </div>
             </div>
-            <div>
-
+            <div  className='mb-12 xl:max-w-[1140px] lg:max-w-[90%] md:max-w-[90%] sm:max-w-[90%] w-[95%] mx-auto md:p-12 shadow'>
+                
             </div>
             
         </>
